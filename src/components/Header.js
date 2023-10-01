a4496f71fb07c8160f9293738798fa53c3664d01
Rizni <mnriznimohamed@gmail.com>
2023-10-01 13:06:38 +0530

    added responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,15 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import Logo from './Logo';
+import { useTheme } from '@emotion/react';
+import { useMediaQuery } from '@mui/material';
 
 const pages = ['Home', 'Real Time', 'Uploads'];
 
-function Header({page,setPage}) {
+function Header({ page, setPage }) {
+
+    const theme = useTheme();
+    const sm = useMediaQuery(theme.breakpoints.down('sm'));
 
     const handlePages = (page) => {
         setPage(page)
@@ -17,44 +22,80 @@ function Header({page,setPage}) {
 
     return (
         <AppBar position="static">
-            <Container maxWidth="xl" sx={{display:"flex", alignItems:"center"}}>
+            <Container maxWidth="xl" sx={{ display: "flex", alignItems: "center", justifyContent: sm && "center", my: sm && 1 }}>
                 <Box component="img" src={Logo} sx={{
                     width: 50,
                     height: 50,
-                    marginRight:1,
-                    bgcolor:"white",
-                    padding:0.5,
-                    borderRadius:1,
-                }}/>
+                    marginRight: 1,
+                    bgcolor: "white",
+                    padding: 0.5,
+                    borderRadius: 1,
+                }} />
                 <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        sx={{
-                            mr: 2,
-                            fontWeight: 700,
-                            color:"white"
-                        }}
-                    >
-                        RSDetector
-                    </Typography>
+                    <Box>
+                        <Typography
+                            noWrap
+                            sx={{
+                                m:0,
+                                p:0,
+                                mr: 2,
+                                fontWeight: 700,
+                                color: "white",
+                                fontSize:22,
+                            }}
+                        >
+                            RSDetector
+                        </Typography>
+                        <Typography
+                            noWrap
+                            sx={{
+                                m: 0,
+                                p: 0,
+                                mr: 2,
+                                fontWeight: 500,
+                                color: "white",
+                                fontStyle:"italic"
 
-                   
-                    <Box sx={{ flexGrow: 1, display:'flex'  }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                onClick={ () => handlePages(page)}
-                                sx={theme => { return { my: 2, color: 'white', borderBottom:"1.5px solid white", mr:1, fontWeight:700, width:90, padding:0, borderRadius:1}}}
-                            >
-                                {page}
-                            </Button>
-                        ))}
+                            }}
+                        >
+                            Detect, Alert, and Safety
+                        </Typography>
                     </Box>
 
 
+                    {!sm && (
+                        <Box sx={{ flexGrow: 1, display: 'flex' }}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    onClick={() => handlePages(page)}
+                                    sx={theme => { return { my: 2, color: 'white', borderBottom: "1.5px solid white", mr: 1, fontWeight: 700, width: 90, padding: 0, borderRadius: 1 } }}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+                    )}
+
                 </Toolbar>
+
+
+
             </Container>
+
+            {sm && (
+                <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: "space-evenly"}}>
+                    {pages.map((page) => (
+                        <Button
+                            key={page}
+                            onClick={() => handlePages(page)}
+                            sx={theme => { return { my: 2, color: 'white', borderBottom: "1.5px solid white", mr: 1, fontWeight: 700, width: 90, padding: 0, borderRadius: 1 } }}
+                        >
+                            {page}
+                        </Button>
+                    ))}
+                </Box>
+            )}
         </AppBar>
     );
 }
--- src/pages/Realtime.js
+++ src/pages/Realtime.js
@@ -1,8 +1,22 @@
+import { Box, Button } from '@mui/material'
 import React from 'react'
 
 const Realtime = () => {
+
+  const handleStart = () => {
+    alert("started")
+  }
+
+
   return (
-    <div>Realtime</div>
+    <Box display="flex" flexDirection="column" height="calc( 100vh - 100px) " alignItems="center" my={10} >
+
+      <Box sx={{ border: "1px solid red", width: [300, 600], height: [200, 500], }} >
+        video
+      </Box>
+
+      <Button variant='contained' size='small' sx={{ my: 2 }} onClick={handleStart}>Start</Button>
+    </Box>
   )
 }
 
