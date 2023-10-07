c5ceb94210119431947efbb1d60aa2c71d7d1787
Rizni <mnriznimohamed@gmail.com>
2023-10-07 19:56:37 +0530

    updated api

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API = axios.create({
     // baseURL: 'http://localhost:8000/V1/',
-    baseURL: 'https://5339-112-134-197-76.ngrok-free.app/V1/',
+    baseURL: 'https://881f-112-134-198-70.ngrok-free.app/V1/',
     
 });
 
@@ -11,7 +11,7 @@ const API = axios.create({
 export const send_blob = async (send_data, token) => {
     try {
         const response = await API.post(
-            `secure-data`,
+            `secure-process`,
             send_data, {
             headers: { Authorization: `Bearer ${token}` },
         });
